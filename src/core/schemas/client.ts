460006a0362838f0c8e2849075521ef8e1d57ace
brunonoatto <brunonoatto@gmail.com>
2024-04-11 15:15:19 -0300

    fix: utilização da lib brazilian-values para validar cpf e ajuste no tamanho do telefone

--- src/core/schemas/client.ts
+++ src/core/schemas/client.ts
@@ -1,29 +1,9 @@
 import { z } from "zod";
+import { isCPF } from "brazilian-values";
 
 export const clientSchema = z
   .object({
-    cpf: z.string().refine((cpf: string) => {
-      if (typeof cpf !== "string") return false;
-
-      cpf = cpf.replace(/[^\d]+/g, "");
-
-      if (cpf.length !== 11 || !!cpf.match(/(\d)\1{10}/)) return false;
-
-      const cpfDigits = cpf.split("").map((el) => +el);
-
-      const rest = (count: number): number => {
-        return (
-          ((cpfDigits
-            .slice(0, count - 12)
-            .reduce((soma, el, index) => soma + el * (count - index), 0) *
-            10) %
-            11) %
-          10
-        );
-      };
-
-      return rest(10) === cpfDigits[9] && rest(11) === cpfDigits[10];
-    }, "CPF Inválido."),
+    cpf: z.string().refine((cpf: string) => isCPF(cpf), "CPF Inválido."),
     name: z
       .string()
       .min(3, "Mínimo de 3 caracteres.")
@@ -31,7 +11,7 @@ export const clientSchema = z
     email: z.string().email("E-mail inválido."),
     phone: z.string().refine(
       // (value) => /^\([1-9]{2}\) (?:9)([0-9]{4})\-([0-9]{4})$/.test(value),
-      (value) => /^[1-9]{2}(?:9)([0-9]{7})$/.test(value),
+      (value) => /^[1-9]{2}(9)([0-9]{8})$/.test(value),
       "Telefone inválido. (00) 90000-0000"
     ),
   })
