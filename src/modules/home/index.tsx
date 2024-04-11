303890592208b2a72c319a004efa56c582a1cba8
brunonoatto <brunonoatto@gmail.com>
2024-04-11 03:01:16 -0300

    fix: ajuste para fazer o refech após deletar um Client

--- src/modules/home/index.tsx
+++ src/modules/home/index.tsx
@@ -22,7 +22,7 @@ import EditOutlinedIcon from "@mui/icons-material/EditOutlined";
 import DeleteOutlineOutlinedIcon from "@mui/icons-material/DeleteOutlineOutlined";
 import AddIcon from "@mui/icons-material/Add";
 
-import { useDeleteClient, useGetUsers } from "@core/services/client";
+import { useDeleteClient, useGetClients } from "@core/services/client";
 import { RoutesPathEnum } from "@core/router/types";
 import { useState } from "react";
 import type { TClient } from "@core/schemas";
@@ -31,7 +31,7 @@ export default function Home() {
   const [modalData, setModalData] = useState<TClient>();
   const navigate = useNavigate();
   const { mutate: mudateDeleteClient } = useDeleteClient();
-  const { data, refetch } = useGetUsers();
+  const { data, refetch } = useGetClients();
 
   const handleAddClientClick = () => {
     navigate(RoutesPathEnum.Cliente);
@@ -48,76 +48,90 @@ export default function Home() {
   const handleConfirmDeleteClientClick = async () => {
     if (!modalData) return;
 
-    mudateDeleteClient(modalData.cpf);
-    refetch();
+    mudateDeleteClient(modalData.cpf, {
+      onSuccess: () => {
+        refetch();
+      },
+      onError: () => {
+        // toaster error
+      },
+      onSettled: () => {
+        setModalData(undefined);
+      },
+    });
   };
 
   return (
-    <Container maxWidth="sm">
-      <Typography variant="h4">Lista de Clientes</Typography>
+    <Container>
+      <Box maxWidth="md" textAlign="center">
+        <Typography variant="h4">Lista de Clientes</Typography>
 
-      <Box textAlign="right">
-        <Button onClick={handleAddClientClick}>
-          <AddIcon /> Adicionar Cliente
-        </Button>
-      </Box>
-
-      <TableContainer component={Paper}>
-        <Table aria-label="simple table">
-          <TableHead>
-            <TableRow>
-              <TableCell>CPF</TableCell>
-              <TableCell>Nome</TableCell>
-              <TableCell>E-mail</TableCell>
-              <TableCell>Telefone</TableCell>
-              <TableCell width={"120px"}></TableCell>
-            </TableRow>
-          </TableHead>
-          <TableBody>
-            {data?.map((client) => (
-              <TableRow
-                key={client.name}
-                sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
-              >
-                <TableCell component="th" scope="row">
-                  {client.cpf}
-                </TableCell>
-                <TableCell>{client.name}</TableCell>
-                <TableCell>{client.email}</TableCell>
-                <TableCell>{client.phone}</TableCell>
-                <TableCell>
-                  <IconButton onClick={handleEditClientClick(client.cpf)}>
-                    <EditOutlinedIcon />
-                  </IconButton>
-                  <IconButton onClick={handleDeleteClientClick(client)}>
-                    <DeleteOutlineOutlinedIcon />
-                  </IconButton>
-                </TableCell>
-              </TableRow>
-            ))}
-          </TableBody>
-        </Table>
-      </TableContainer>
-
-      {data && data.length <= 0 && (
-        <Box textAlign="center" sx={{ mt: 2 }}>
-          Nenhum Cliente cadastrado
-        </Box>
-      )}
-
-      <Dialog open={!!modalData} onClose={() => setModalData(undefined)}>
-        <DialogTitle>Confirmação</DialogTitle>
-        <DialogContent>
-          <DialogContentText>
-            Confirma a exclusão do usuário {modalData?.name}?
-          </DialogContentText>
-        </DialogContent>
-        <DialogActions>
-          <Button variant="contained" onClick={handleConfirmDeleteClientClick}>
-            Confirmar
+        <Box textAlign="right">
+          <Button onClick={handleAddClientClick}>
+            <AddIcon /> Adicionar Cliente
           </Button>
-        </DialogActions>
-      </Dialog>
+        </Box>
+
+        <TableContainer component={Paper}>
+          <Table aria-label="simple table">
+            <TableHead>
+              <TableRow>
+                <TableCell>CPF</TableCell>
+                <TableCell>Nome</TableCell>
+                <TableCell>E-mail</TableCell>
+                <TableCell>Telefone</TableCell>
+                <TableCell width={"120px"}></TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {data?.map((client) => (
+                <TableRow
+                  key={client.name}
+                  sx={{ "&:last-child td, &:last-child th": { border: 0 } }}
+                >
+                  <TableCell component="th" scope="row">
+                    {client.cpf}
+                  </TableCell>
+                  <TableCell>{client.name}</TableCell>
+                  <TableCell>{client.email}</TableCell>
+                  <TableCell>{client.phone}</TableCell>
+                  <TableCell>
+                    <IconButton onClick={handleEditClientClick(client.cpf)}>
+                      <EditOutlinedIcon />
+                    </IconButton>
+                    <IconButton onClick={handleDeleteClientClick(client)}>
+                      <DeleteOutlineOutlinedIcon />
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </TableContainer>
+
+        {data && data.length <= 0 && (
+          <Box textAlign="center" sx={{ mt: 2 }}>
+            Nenhum Cliente cadastrado
+          </Box>
+        )}
+
+        <Dialog open={!!modalData} onClose={() => setModalData(undefined)}>
+          <DialogTitle>Confirmação</DialogTitle>
+          <DialogContent>
+            <DialogContentText>
+              Confirma a exclusão do usuário {modalData?.name}?
+            </DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button
+              variant="contained"
+              onClick={handleConfirmDeleteClientClick}
+            >
+              Confirmar
+            </Button>
+          </DialogActions>
+        </Dialog>
+      </Box>
     </Container>
   );
 }
