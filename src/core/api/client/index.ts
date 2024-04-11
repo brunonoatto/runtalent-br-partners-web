5cf75a88d44a7f7594893c64123af48eb22a047c
brunonoatto <brunonoatto@gmail.com>
2024-04-11 00:15:03 -0300

    feat: implementação das chamadas da api

--- src/core/api/client/index.ts
+++ src/core/api/client/index.ts
@@ -1,5 +1,35 @@
-export const get = () => {
-  fetch("http://localhost:5173/client").then((response) => {
-    console.log({ response });
+import { TClient } from "@core/schemas";
+
+const URL_BASE = "http://localhost:5173/client";
+
+export const get = async () => {
+  return fetch(URL_BASE).then<TClient[]>((response) => {
+    return response.json();
+  });
+};
+
+export const post = async (data: TClient) => {
+  return fetch(URL_BASE, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+export const update = async (data: TClient) => {
+  return fetch(URL_BASE, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+};
+
+export const remove = async (cpf: string) => {
+  return fetch(`${URL_BASE}/${cpf}`, {
+    method: "DELETE",
   });
 };
