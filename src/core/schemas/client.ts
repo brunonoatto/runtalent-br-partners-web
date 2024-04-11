e79f270e4b1da4adfb76a220bea9a0a0d817e669
brunonoatto <brunonoatto@gmail.com>
2024-04-10 23:11:10 -0300

    fix: ajuste na máscara do cpf

--- src/core/schemas/client.ts
+++ src/core/schemas/client.ts
@@ -24,12 +24,11 @@ export const clientSchema = z
       .min(3, "Mínimo de 3 caracteres.")
       .max(100, "Máximo de 100 caracteres."),
     email: z.string().email("E-mail inválido."),
-    phone: z
-      .string()
-      .refine(
-        (value) => /^\([1-9]{2}\) (?:9)([0-9]{4})\-([0-9]{4})$/.test(value),
-        "Telefone inválido. (00) 90000-0000"
-      ),
+    phone: z.string().refine(
+      // (value) => /^\([1-9]{2}\) (?:9)([0-9]{4})\-([0-9]{4})$/.test(value),
+      (value) => /^[1-9]{2}(?:9)([0-9]{8})$/.test(value),
+      "Telefone inválido. (00) 90000-0000"
+    ),
   })
   .strict();
 
