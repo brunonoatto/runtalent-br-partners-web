5c61f2e7fccc534a00c44cb94bef96623e3954b9
brunonoatto <brunonoatto@gmail.com>
2024-04-11 21:21:52 -0300

    feat: adaptação ao novo retorno da api e formatando o objeto Client que retorna do useGetClients

--- src/core/services/client.ts
+++ src/core/services/client.ts
@@ -1,12 +1,18 @@
 import Api from "@core/api";
+import { TResponseBase } from "@core/api/types";
+import Client from "@core/models/client";
 import type { TClient } from "@core/schemas";
-import { useMutation, useQuery } from "@tanstack/react-query";
+import { DefaultError, useMutation, useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 
 export const useGetClients = () => {
   return useQuery({
     queryKey: ["useGetClients"],
-    queryFn: Api.Client.get,
+    queryFn: async () => {
+      const { data } = await Api.Client.get();
+
+      return data?.map((c) => new Client(c)) || [];
+    },
   });
 };
 
@@ -19,26 +25,26 @@ export const useGetClient = () => {
     queryFn: async () => {
       if (!cpfParam) return null;
 
-      const clients = await Api.Client.getClient(cpfParam);
-      return clients;
+      const { data } = await Api.Client.getClient(cpfParam);
+      return data;
     },
   });
 };
 
 export const usePostClient = () => {
-  return useMutation<unknown, unknown, TClient>({
+  return useMutation<TResponseBase<boolean>, DefaultError, TClient>({
     mutationFn: Api.Client.post,
   });
 };
 
 export const useUpdateClient = () => {
-  return useMutation<unknown, unknown, TClient>({
+  return useMutation<TResponseBase<boolean>, DefaultError, TClient>({
     mutationFn: Api.Client.update,
   });
 };
 
 export const useDeleteClient = () => {
-  return useMutation<unknown, unknown, string>({
+  return useMutation<TResponseBase<boolean>, DefaultError, string>({
     mutationFn: Api.Client.remove,
   });
 };
